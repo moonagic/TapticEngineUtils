d878487f8fa0fb0390bdda935d7cb05d2611eab8
moonagic <i@moonagic.com>
2021-08-16 14:05:31 +0800

    兼容NotificationFeedback

--- Sources/TapticEngineUtils/TapticEngineUtils.swift
+++ Sources/TapticEngineUtils/TapticEngineUtils.swift
@@ -9,16 +9,26 @@ import UIKit
 
 @available(iOS 10.0, *)
 public class TapticEngineUtils {
+    
     private static var currentStyle = UIImpactFeedbackGenerator.FeedbackStyle.light
     private static var tapticEngine = UIImpactFeedbackGenerator(style: currentStyle)
     
     public static var tapticEngineEnabled = true
     
-    public final class func feedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    public final class func feedback(style: UnclearFeedbackStyle) {
         if !tapticEngineEnabled { return }
-        prepare(style: style)
-        tapticEngine.impactOccurred()
-        tapticEngine.prepare()
+        switch style {
+        case .impact(let style):
+            prepare(style: style)
+            UINotificationFeedbackGenerator().notificationOccurred(UINotificationFeedbackGenerator.FeedbackType.success)
+            tapticEngine.impactOccurred()
+            tapticEngine.prepare()
+            break
+        case .notification(let style):
+            UINotificationFeedbackGenerator().notificationOccurred(style)
+            break
+        }
+        
     }
     
     public final class func prepare(style: UIImpactFeedbackGenerator.FeedbackStyle) {
@@ -28,3 +38,9 @@ public class TapticEngineUtils {
         }
     }
 }
+
+public enum UnclearFeedbackStyle {
+    case impact(style: UIImpactFeedbackGenerator.FeedbackStyle)
+    case notification(style: UINotificationFeedbackGenerator.FeedbackType)
+}
+
